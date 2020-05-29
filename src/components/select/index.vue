a6eea0879c4b64d80f30b1f484cefa355cf2e37c
easy909 <1730127479@qq.com>
2020-05-29 14:24:12 +0800

    人员管理

--- src/components/select/index.vue
+++ src/components/select/index.vue
@@ -1,7 +1,12 @@
 <!--  -->
 <template>
-  <el-select v-model="value" placeholder="请选择">
-    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+  <el-select v-model="data.search_key" style="width:100%">
+    <el-option
+      v-for="item in data.initOption"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    ></el-option>
   </el-select>
 </template>
 
@@ -13,64 +18,65 @@ export default {
   name: "selectIndex",
   //import引入的组件需要注入到对象中才能使用
   props: {
-    config:{
-      type:Array,
-      default:[]
+    config: {
+      type: Array,
+      default: []
     }
   },
   data() {
     //这里存放数据
     return {
-      options: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
-      ],
-      value: ""
+      data: {
+        search_key: "",
+        search_keyWork: "",
+        initOption: [],
+        options: [
+          {
+            value: "name",
+            label: "姓名"
+          },
+          {
+            value: "phone",
+            label: "手机号"
+          },
+          {
+            value: "email",
+            label: "邮箱"
+          },
+          {
+            value: "id",
+            label: "ID"
+          },
+          {
+            value: "title",
+            label: "标题"
+          }
+        ]
+      }
     };
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
-  watch: {},
+  watch: {
+  
+  },
   //方法集合
   methods: {
-    initConfig(){
-      this.config.forEach((item,index)=>{
-        
-      })
+    initConfig() {
+      let optionArray = [];
+      this.config.forEach(items => {
+        let arr = this.data.options.filter(item => item.value == items);
+        if (arr.length !== 0) optionArray.push(arr[0]);
+      });
+      this.data.initOption = optionArray;
+      this.data.search_key = optionArray[0];
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
-    this.initConfig()
+    this.initConfig();
   },
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
-  beforeDestroy() {}, //生命周期 - 销毁之前
-  destroyed() {}, //生命周期 - 销毁完成
-  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
 <style lang='scss' scoped>
